c4a18c8d75313465935a2afa6b4b2402f7a26149
Indervir Singh <iindervirsingh@gmail.com>
2021-02-25 15:48:27 -0800

    started working on the readme generator by declaring the variables and functions beforehand, so I know what is needed and then what needs to be done

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+/* 
+Variables ------------------------------------------------------------------------------*/
+    const inquirer = require('inquirer');
+    const fs = require('fs');
 
+    const questions = [];
+//
+
+
+
+/* 
+Functions ------------------------------------------------------------------------------*/
+
+    // - Initalize the app
+    const init = () => {
+    }
+
+    // - Write the README.md file
+    const writeToFile = (fileName, data) => {
+    }
+
+    // - Create the license section
+    const renderLicenseSection = (license) => {
+    }
+
+    // - Create the license link
+    const renderLicenseLink = (license) => {
+    }
+
+    // - Create license badge
+    const renderLicenseBadge = (license) => {
+    }
+
+    // - Generate the README.md file
+    const generateMarkdown = (data) => {
+        return `# ${data.title}`;
+    }
+
+//
