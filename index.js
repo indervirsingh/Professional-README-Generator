846640452a2a781c4ddd441bbab3c3d30b35b842
Indervir Singh <iindervirsingh@gmail.com>
2021-02-25 16:40:48 -0800

    created a prompt with the questions for generating a readme.md file, still need to generate the markdown file based on the answers

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 Variables ------------------------------------------------------------------------------*/
     const inquirer = require('inquirer');
     const fs = require('fs');
-
-    const questions = [];
 //
 
 
@@ -13,27 +11,97 @@ Functions ----------------------------------------------------------------------
 
     // - Initalize the app
     const init = () => {
-    }
+
+        // First get the user's details
+        promptUser()
+            .then(writeToFile('README.md', generateMarkdown(answers)))
+            .then(() => console.log('Congrats! A README.md file has been generated.'))
+            .catch((error) => console.log(error));
+    };
+
+    // - Prompt for the README.md generator
+    const promptUser = () => {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Please enter the Title/Name of your project :'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Please enter a Description for your project :'
+            },
+            {
+                type: 'list',
+                name: 'tableOfContentsConfirm',
+                message: 'Would you like to include a Table of Contents? (OPTIONAL)',
+                choices: ['Yes', 'No']
+            },
+            {
+                type: 'input',
+                name: 'installationInstructions',
+                message: 'Please enter step-by-step instructions on how to Install your project :'
+            },
+            {
+                type: 'input',
+                name: 'usageInstructions',
+                message: 'Please enter what your recommended Usages are, or any examples you would like to link :'
+            },
+            {
+                type: 'input',
+                name: 'collaborators',
+                message: 'Please enter all the collaborators (including yourself) using this format - [GitHub_username](link_to_GitHub) :'
+            },
+            {
+                type: 'list',
+                name: 'testsConfirm',
+                message: 'Would you like to include any test links of this project?',
+                choices: ['Yes', 'No']
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'If you have any test links, please enter them here using this format - [test_name](test_link) :'
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Please select which License you would like to include :',
+                choices: ['MIT License', 'GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License', 'Unlicense License']
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Please enter your GitHub Username :'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Please enter your Email Address :'
+            }
+        ]);
+    };
 
     // - Write the README.md file
     const writeToFile = (fileName, data) => {
-    }
+    };
 
     // - Create the license section
     const renderLicenseSection = (license) => {
-    }
+    };
 
     // - Create the license link
     const renderLicenseLink = (license) => {
-    }
+    };
 
     // - Create license badge
     const renderLicenseBadge = (license) => {
-    }
+    };
 
     // - Generate the README.md file
     const generateMarkdown = (data) => {
         return `# ${data.title}`;
-    }
+    };
 
 //
